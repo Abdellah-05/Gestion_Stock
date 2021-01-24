b91d2fcf9c444e81380a0b798bdf5c8052bea563
Abdellah ELAAROUB <abdouelaaroub@gmail.com>
2021-01-24 18:20:14 +0100

    login code

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 from flask import Flask, render_template, request, redirect, session
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+#from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import datetime
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo   # pip install Flask-PyMongo
 
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/gestion_stock"
 mongo = PyMongo(app)
 
-admin1 = mongo.db.admin.find_one_or_404({"_id": 1})
-admin2 = mongo.db.admin.find_one_or_404({"_id": 2})
-print('---------------------------------------------------')
-print('test connexion database  ---- ')
-print("admin1 --->  "+ admin1["nom"]+ ' ' + admin1["prenom"])
-print("admin2 --->  "+ admin2["nom"]+ ' ' + admin2["prenom"]) 
+def isEmpty(word):
+    if word:
+        return False
+    return True
+
+
+@app.route('/', methods = ['GET'] )
+def loginGet():    
+    return render_template('login.html')
+
+@app.route('/', methods = ['POST'] )
+def loginPost():
+    login = request.form['login']
+    password = request.form['password']
+
+    admin = mongo.db.admin.find_one({"gmail":login, "password": password})
+
+    #login_user(adminEmail)
+
+    if isEmpty(admin): 
+        return redirect('/')
+    else :
+        return redirect('/ourStock')
  
